3ce8ab051db6360fa72fbaa1f8632a4cd0d28eb5
mefistofelus <manilo@ukr.net>
2020-07-24 03:08:11 +0300

    верстка сайдбара

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,18 @@
 //= jquery-1.11.0.min.js
+//= jquery-migrate-1.2.1.min.js
 //= ../../node_modules/slick-carousel/slick/slick.min.js
+
+// Slick slider init
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    pauseOnHover: false,
+    // adaptiveHeight: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '<button class="slick-prev"><span class="btn-icon btn-left"></span></button>',
+    nextArrow: '<button class="slick-next"><span class="btn-icon btn-right"></span></button>',
+  });
